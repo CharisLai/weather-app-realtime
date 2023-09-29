071567a9b16eb5e1ec29311cf6eeaf22999bf00b
CharisLai <griffin8418@gmail.com>
2023-09-29 13:46:09 +0800

    feat: install emotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// Step 1 載入 emotion styled套件
+import styled from '@emotion/styled'
 
-function App() {
+
+// Step 2 定義帶有styled 的元件
+const Container = styled.div`
+	background-color: #ededed;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+	position: relative;
+	min-width: 360px;
+	box-shadow: 0 1px 3px 0 #999999;
+	background-color: #f9f9f9;
+	box-sizing: border-box;
+	padding: 30px 15px;
+`;
+
+// Step 3 把上定義好的 styled-component 當成元件使用
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <WeatherCard>
+        <h1>Weather</h1>
+      </WeatherCard>
+    </Container>
+  )
 }
 
-export default App;
+export default App
